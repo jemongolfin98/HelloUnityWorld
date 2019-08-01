43898b8c706be9dd88e923bf4565c43bc2aff8b4
jemongolfin98 <jemongolfin98@users.noreply.github.com>
2019-08-01 12:43:46 -0400

    first commit

---
+++ Hello Unity World/Assets/ChangeName.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChangeName : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            TextMeshProUGUI textmeshPro = GetComponent<TextMeshProUGUI>();
+            textmeshPro.SetText("Hello World from Jemon Golfin!");
+        }
+
+        if(Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
